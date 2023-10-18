22fa85b8cc31032f2878b784f3e1fd562aacf156
Lei Yang <lei.yang@fadv.com>
2023-10-18 11:21:34 -0400

    Removed the code.

---
+++ src/main/java/me/ehp246/aufkafka/core/reflection/ReflectedMethod.java
@@ -0,0 +1,85 @@
+package me.ehp246.aufkafka.core.reflection;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * @author Lei Yang
+ *
+ */
+public final class ReflectedMethod {
+    private final Class<?> declaringType;
+    private final Method method;
+    private final Parameter[] parameters;
+    private final List<Class<?>> exceptionTypes;
+
+    public ReflectedMethod(final Method method) {
+        this.method = Objects.requireNonNull(method);
+        this.declaringType = method.getDeclaringClass();
+        this.parameters = method.getParameters();
+        this.exceptionTypes = List.of(method.getExceptionTypes());
+    }
+
+    public Optional<ReflectedParameter> firstPayloadParameter(final Set<Class<? extends Annotation>> exclusions) {
+        for (var i = 0; i < parameters.length; i++) {
+            final var parameter = parameters[i];
+            if (exclusions.stream().filter(type -> parameter.isAnnotationPresent(type)).findAny().isEmpty()) {
+                return Optional.of(new ReflectedParameter(parameter, i));
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public List<ReflectedParameter> allParametersWith(final Class<? extends Annotation> annotationType) {
+        final var list = new ArrayList<ReflectedParameter>();
+
+        for (int i = 0; i < parameters.length; i++) {
+            final var parameter = parameters[i];
+            if (parameter.isAnnotationPresent(annotationType)) {
+                list.add(new ReflectedParameter(parameter, i));
+            }
+        }
+
+        return list;
+    }
+
+    public Method method() {
+        return this.method;
+    }
+
+    public Parameter getParameter(final int index) {
+        return this.parameters[index];
+    }
+
+    public <A extends Annotation> Optional<A> findOnMethodUp(final Class<A> annotationClass) {
+        final var found = method.getAnnotation(annotationClass);
+        if (found != null) {
+            return Optional.of(found);
+        }
+
+        return Optional.ofNullable(declaringType.getAnnotation(annotationClass));
+    }
+
+    /**
+     * Is the given type on the <code>throws</code>. Must be explicitly declared.
+     * Not on the clause doesn't mean the exception can not be thrown by the method,
+     * e.g., all runtime exceptions.
+     */
+    public boolean isOnThrows(final Class<?> type) {
+        return RuntimeException.class.isAssignableFrom(type)
+                || this.exceptionTypes.stream().filter(t -> t.isAssignableFrom(type)).findAny().isPresent();
+    }
+
+    public boolean returnsVoid() {
+        final var returnType = method.getReturnType();
+
+        return returnType == void.class || returnType == Void.class;
+    }
+}
---
+++ src/main/java/me/ehp246/aufkafka/core/reflection/ReflectedType.java
@@ -0,0 +1,90 @@
+package me.ehp246.aufkafka.core.reflection;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author Lei Yang
+ * @since 1.0
+ */
+public final class ReflectedType<T> {
+    private final Class<T> type;
+    private final List<Method> methods;
+
+    public ReflectedType(final Class<T> type) {
+        super();
+        this.type = type;
+        this.methods = List.of(type.getDeclaredMethods());
+    }
+
+    public static <T> ReflectedType<T> reflect(final Class<T> type) {
+        return new ReflectedType<T>(type);
+    }
+
+    /**
+     * Returns the named method that does not have any parameter. Returns null if
+     * not found.
+     *
+     * @param name
+     * @return
+     */
+    public Method findMethod(final String name) {
+        try {
+            return type.getMethod(name, (Class<?>[]) null);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public Method findMethod(final String name, final Class<?>... parameters) {
+        try {
+            return type.getMethod(name, parameters);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns all methods that have the given name ignoring the parameters.
+     *
+     * @param name
+     * @return
+     */
+    public List<Method> findMethods(final String name) {
+        return Stream.of(type.getMethods()).filter(method -> method.getName().equals(name))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns all methods that have the given annotation.
+     *
+     * @param annotationClass
+     * @return
+     */
+    public List<Method> findMethods(final Class<? extends Annotation> annotationClass) {
+        return Stream.of(type.getMethods()).filter(method -> method.getDeclaredAnnotation(annotationClass) != null)
+                .collect(Collectors.toList());
+    }
+
+    public Class<T> getType() {
+        return type;
+    }
+
+    public <A extends Annotation> Optional<A> findOnType(final Class<A> annotationType) {
+        return Optional.ofNullable(type.getAnnotation(annotationType));
+    }
+
+    public Stream<Method> streamSuppliersWith(final Class<? extends Annotation> annotationClass) {
+        return this.streamMethodsWith(annotationClass).filter(m -> m.getParameterCount() == 0
+                && (m.getReturnType() != void.class && m.getReturnType() != Void.class));
+    }
+
+    public Stream<Method> streamMethodsWith(final Class<? extends Annotation> annotationType) {
+        return methods.stream().filter(method -> method.isAnnotationPresent(annotationType));
+    }
+}
