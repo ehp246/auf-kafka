c3506322817d798fe7a4896f9880beea51277762
Lei Yang <ehp246@yahoo.com>
2023-10-20 10:13:11 -0400

    Evnt Hubs producer.

--- src/test/java/me/ehp246/test/eventhubs/producer/basic/AppConfig.java
+++ src/test/java/me/ehp246/test/eventhubs/producer/basic/AppConfig.java
@@ -1,6 +1,16 @@
 package me.ehp246.test.eventhubs.producer.basic;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Lei Yang
@@ -8,4 +18,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 class AppConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String jaasConfig;
+
+    @Bean
+    Producer<String, String> createProducer() throws FileNotFoundException, IOException {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put("sasl.jaas.config", jaasConfig);
+        properties.put("security.protocol", "SASL_SSL");
+        properties.put("sasl.mechanism", "PLAIN");
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "aufkafka-local-eventhubs");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return new KafkaProducer<>(properties);
+    }
 }
--- src/test/java/me/ehp246/test/eventhubs/producer/basic/BasicTest.java
+++ src/test/java/me/ehp246/test/eventhubs/producer/basic/BasicTest.java
@@ -1,25 +1,43 @@
 package me.ehp246.test.eventhubs.producer.basic;
 
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.Uuid;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * @author Lei Yang
  *
  */
 @SpringBootTest(classes = { AppConfig.class }, webEnvironment = WebEnvironment.NONE)
+@ActiveProfiles("local")
 @EnabledIfSystemProperty(named = "me.ehp246.test.eventhubs", matches = "enabled")
 class BasicTest {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    private Producer<String, String> producer;
 
     @Test
-    void basic_01() {
+    void template_01() {
         kafkaTemplate.send("basic", "NewEvent", Uuid.randomUuid().toString());
+    }
+
+    @Test
+    void producer_01() throws InterruptedException, ExecutionException {
+        final var future = producer.send(new ProducerRecord<String, String>("basic", "NewEvent", Uuid.randomUuid().toString()));
+
+        final var recordMetadata = future.get();
+
+        Assertions.assertEquals(0, recordMetadata.partition());
     }
 }
