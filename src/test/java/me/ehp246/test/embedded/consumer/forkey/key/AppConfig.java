81923e7dc015c344980f55f2a5971e245e3e3dd0
Lei Yang <ehp246@yahoo.com>
2023-12-26 17:03:57 -0500

    Key test.

---
+++ src/test/java/me/ehp246/test/embedded/consumer/forkey/key/AppConfig.java
@@ -0,0 +1,17 @@
+package me.ehp246.test.embedded.consumer.forkey.key;
+
+import org.springframework.kafka.annotation.EnableKafka;
+
+import me.ehp246.aufkafka.api.annotation.EnableForKafka;
+import me.ehp246.aufkafka.api.annotation.EnableForKafka.Inbound;
+import me.ehp246.aufkafka.api.annotation.EnableForKafka.Inbound.From;
+import me.ehp246.test.mock.WildcardAction;
+
+/**
+ * @author Lei Yang
+ *
+ */
+@EnableKafka
+@EnableForKafka({ @Inbound(value = @From("embedded"), register = WildcardAction.class) })
+class AppConfig {
+}
