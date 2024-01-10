866752c0444bacf5c9a70cd55fb923691ef5b09b
Lei Yang <ehp246@yahoo.com>
2024-01-10 09:29:10 -0500

    Javadoc update.

--- src/main/java/me/ehp246/aufkafka/api/annotation/ByKafka.java
+++ src/main/java/me/ehp246/aufkafka/api/annotation/ByKafka.java
@@ -7,14 +7,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 import me.ehp246.aufkafka.api.producer.DirectPartitionMap;
 import me.ehp246.aufkafka.api.producer.PartitionMap;
 import me.ehp246.aufkafka.api.producer.SerializedPartitionMap;
 
 /**
- * 
+ * Indicates that the annotated interface should be implemented by Auf Kafka as
+ * a message {@linkplain Producer producer} and made available for injection.
+ * <p>
+ * All out-bound messages are of {@linkplain ProducerRecord
+ * ProducerRecord&lt;String, String&gt;}.
+ * <p>
+ * Serializing by {@linkplain JsonView} is supported on the body.
  * 
  * @author Lei Yang
  * @since 1.0
@@ -24,7 +34,8 @@ import me.ehp246.aufkafka.api.producer.SerializedPartitionMap;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ByKafka {
     /**
-     * Specifies the destination name for out-bound messages.
+     * Specifies the destination name for out-bound {@linkplain ProducerRecord
+     * messages}.
      */
     String value();
 
@@ -40,12 +51,13 @@ public @interface ByKafka {
     String name() default "";
 
     /**
-     * Specifies header name/value pairs for out-going messages.
+     * Specifies {@linkplain ProducerRecord#headers() header} key/value pairs for
+     * out-going messages.
      * <p>
      * E.g.,
      * <p>
      * <code>
-     *     { "AppName", "AufJms", "AppVersion", "1.0", ... }
+     *     { "AppName", "AufKafka", "AppVersion", "1.0", ... }
      * </code>
      * <p>
      * Must be specified in pairs. Missing value will trigger an exception.
@@ -57,7 +69,7 @@ public @interface ByKafka {
      *     { "AppVersion" }
      * </code>
      * <p>
-     * Spring property placeholder is supported on values but not on names.
+     * Spring property placeholder is supported on values but not on keys.
      *
      */
     String[] headers() default {};
