0b9419ee62dba754c1a30c5d9c8bc1c64f989b14
Lei Yang <ehp246@yahoo.com>
2025-04-04 11:22:46 -0400

    Clean up.

--- src/test/java/me/ehp246/test/embedded/consumer/enable/basic/topic/TopicTest.java
+++ src/test/java/me/ehp246/test/embedded/consumer/enable/basic/topic/TopicTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
@@ -18,8 +19,8 @@ import me.ehp246.test.mock.WildcardAction;
  * @author Lei Yang
  *
  */
-@SpringBootTest(classes = { EmbeddedKafkaConfig.class, AppConfig.class, WildcardAction.class },
-        properties = { "topic2=embedded.2" })
+@SpringBootTest(classes = { EmbeddedKafkaConfig.class, AppConfig.class, WildcardAction.class }, properties = {
+        "topic2=embedded.2" }, webEnvironment = WebEnvironment.NONE)
 @EmbeddedKafka(topics = { "embedded.1", "embedded.2" }, partitions = 1)
 @DirtiesContext
 class TopicTest {
