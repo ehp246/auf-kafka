bd0da3822dc460b50b1e922a09e821ee6821ff57
Lei Yang <ehp246@yahoo.com>
2025-04-15 14:12:08 -0400

    Document update.

--- src/main/java/me/ehp246/aufkafka/api/annotation/ByKafka.java
+++ src/main/java/me/ehp246/aufkafka/api/annotation/ByKafka.java
@@ -35,76 +35,78 @@ import me.ehp246.aufkafka.api.producer.SerializedPartitionMap;
 @Target(TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ByKafka {
-	/**
-	 * Specifies the destination name for out-bound {@linkplain ProducerRecord
-	 * messages}.
-	 */
-	String value();
+    /**
+     * Specifies the destination name for out-bound {@linkplain ProducerRecord
+     * messages}.
+     */
+    String value();
 
-	/**
-	 * Specifies a bean name by which the interface can be injected.
-	 * <p>
-	 * The default is from {@link Class#getSimpleName()} with the first letter in
-	 * lower-case.
-	 *
-	 * @return the bean name of the interface.
-	 * @see Qualifier
-	 */
-	String name() default "";
+    /**
+     * Specifies a bean name by which the interface can be injected.
+     * <p>
+     * The default is from {@link Class#getSimpleName()} with the first letter in
+     * lower-case.
+     *
+     * @return the bean name of the interface.
+     * @see Qualifier
+     */
+    String name() default "";
 
-	/**
-	 * Specifies {@linkplain ProducerRecord#headers() header} key/value pairs for
-	 * out-going messages.
-	 * <p>
-	 * E.g.,
-	 * <p>
-	 * <code>
-	 *     { "AppName", "AufKafka", "AppVersion", "1.0", ... }
-	 * </code>
-	 * <p>
-	 * Must be specified in pairs. Missing value will trigger an exception.
-	 * <p>
-	 * E.g., the following is missing value for property '{@code appVersion}' and
-	 * will result an exception.
-	 * <p>
-	 * <code>
-	 *     { "AppVersion" }
-	 * </code>
-	 * <p>
-	 * Spring property placeholder is supported on values but not on keys.
-	 *
-	 */
-	String[] headers() default {};
+    /**
+     * Specifies {@linkplain ProducerRecord#headers() header} key/value pairs for
+     * out-going messages.
+     * <p>
+     * E.g.,
+     * <p>
+     * <code>
+     *     { "AppName", "AufKafka", "AppVersion", "1.0", ... }
+     * </code>
+     * <p>
+     * Must be specified in pairs. Missing value will trigger an exception.
+     * <p>
+     * E.g., the following is missing value for property '{@code appVersion}' and
+     * will result an exception.
+     * <p>
+     * <code>
+     *     { "AppVersion" }
+     * </code>
+     * <p>
+     * Spring property placeholder and SpEL expression are supported on values but
+     * not on keys.
+     *
+     */
+    String[] headers() default {};
 
-	/**
-	 * Specifies the name to pass to {@linkplain ProducerConfigProvider} to retrieve
-	 * producer configuration with which to create a {@linkplain Producer}.
-	 */
-	String producerConfigName() default "";
+    /**
+     * Specifies the name to pass to {@linkplain ProducerConfigProvider} to retrieve
+     * producer configuration with which to create a {@linkplain Producer}.
+     */
+    String producerConfigName() default "";
 
-	/**
-	 * Defines {@linkplain KafkaProducer} property names and values in pairs. E.g.,
-	 * <p>
-	 * <code>
-	 *     { "buffer.memory", "0", ... }
-	 * </code>
-	 * <p>
-	 * These properties will be passed to
-	 * {@linkplain KafkaProducer#KafkaProducer(java.util.Map)}.
-	 * <p>
-	 * Must be specified in pairs. Missing value will trigger an exception.
-	 * <p>
-	 * Spring property placeholder is supported on values but not on names.
-	 *
-	 */
-	String[] producerProperties() default {};
+    /**
+     * Defines {@linkplain KafkaProducer} property names and values in pairs. E.g.,
+     * <p>
+     * <code>
+     *     { "buffer.memory", "0", ... }
+     * </code>
+     * <p>
+     * These properties will be passed to
+     * {@linkplain KafkaProducer#KafkaProducer(java.util.Map)}.
+     * <p>
+     * Must be specified in pairs. Missing value will trigger an exception.
+     * <p>
+     * Spring property placeholder and SpEL expression are supported on values but
+     * not on names.
+     *
+     */
+    String[] producerProperties() default {};
 
-	/**
-	 * Specifies the type of Spring bean that implements {@linkplain PartitionMap}
-	 * to use with the interface to map partition key to partition.
-	 * 
-	 * @see SerializedPartitionMap
-	 * @see DirectPartitionMap
-	 */
-	Class<? extends PartitionMap> partitionMap() default SerializedPartitionMap.class;
+    /**
+     * Specifies the type of Spring bean that implements {@linkplain PartitionMap}
+     * to use with the interface to map partition key to partition.
+     * 
+     * @see SerializedPartitionMap
+     * @see DirectPartitionMap
+     */
+    Class<? extends PartitionMap> partitionMap() default SerializedPartitionMap.class;
 }
