2e14a28c50ceda9c09683ea0077a644db709fdaf
Lei Yang <ehp246@yahoo.com>
2025-06-03 13:22:43 -0400

    All tests passing.

--- src/test/java/me/ehp246/test/embedded/producerfn/ProducerFnTest.java
+++ src/test/java/me/ehp246/test/embedded/producerfn/ProducerFnTest.java
@@ -51,7 +51,7 @@ class ProducerFnTest {
 	final var event = listener.take();
 
 	Assertions.assertEquals(expected, event.key());
-	Assertions.assertEquals(expected, sent.get().record().key());
-	Assertions.assertEquals(AppConfig.TOPIC, sent.get().metadata().topic());
+	Assertions.assertEquals(expected, sent.record().key());
+	Assertions.assertEquals(AppConfig.TOPIC, sent.future().get().topic());
     }
 }
