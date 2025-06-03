f35a8a62f693145e4f39f073ff944aab50d6e3ed
Lei Yang <ehp246@yahoo.com>
2025-06-03 17:19:32 -0400

    In progress.

--- src/main/java/me/ehp246/aufkafka/core/consumer/DefaultInboundEndpointConsumer.java
+++ src/main/java/me/ehp246/aufkafka/core/consumer/DefaultInboundEndpointConsumer.java
@@ -7,6 +7,7 @@ import java.util.function.Supplier;
 import java.util.stream.StreamSupport;
 
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,71 +39,82 @@ final class DefaultInboundEndpointConsumer implements InboundEndpointConsumer {
     private final CompletableFuture<Boolean> closedFuture = new CompletableFuture<Boolean>();
 
     DefaultInboundEndpointConsumer(final Consumer<String, String> consumer,
-            final Supplier<Duration> pollDurationSupplier, final EventInvocableRunnableBuilder dispatcher,
-            final InvocableFactory invocableFactory,
-            final List<InboundConsumerListener.DispatchingListener> onDispatching, final UnmatchedConsumer onUnmatched,
-            final ConsumerExceptionListener onException) {
-        super();
-        this.consumer = consumer;
-        this.pollDurationSupplier = pollDurationSupplier;
-        this.runnableBuilder = dispatcher;
-        this.invocableFactory = invocableFactory;
-        this.onDispatching = onDispatching == null ? List.of() : onDispatching;
-        this.onUnmatched = onUnmatched;
-        this.onException = onException;
+	    final Supplier<Duration> pollDurationSupplier, final EventInvocableRunnableBuilder dispatcher,
+	    final InvocableFactory invocableFactory,
+	    final List<InboundConsumerListener.DispatchingListener> onDispatching, final UnmatchedConsumer onUnmatched,
+	    final ConsumerExceptionListener onException) {
+	super();
+	this.consumer = consumer;
+	this.pollDurationSupplier = pollDurationSupplier;
+	this.runnableBuilder = dispatcher;
+	this.invocableFactory = invocableFactory;
+	this.onDispatching = onDispatching == null ? List.of() : onDispatching;
+	this.onUnmatched = onUnmatched;
+	this.onException = onException;
     }
 
     public void run() {
-        while (!this.closed) {
-            final var polled = consumer.poll(pollDurationSupplier.get());
-            if (polled.count() > 1) {
-                LOGGER.atWarn().setMessage("Polled count: {}").addArgument(polled::count).log();
-            }
+	while (!this.closed) {
+	    try {
+		poll();
+	    } catch (WakeupException e) {
+		if (this.closed) {
+		    LOGGER.atTrace().setCause(e).setMessage("Woke up to close. Ignored").log();
+		} else {
+		    LOGGER.atError().setCause(e).setMessage("Wrongly woke up").log();
+		}
+	    }
+	}
 
-            StreamSupport.stream(polled.spliterator(), false).map(InboundEvent::new).forEach(event -> {
-                try (final var closeble = EventMdcContext.set(event);) {
-                    this.onDispatching.stream().forEach(l -> l.onDispatching(event));
+	this.consumer.close();
+	this.closedFuture.complete(true);
+    }
 
-                    final var invocable = invocableFactory.get(event);
+    private void poll() {
+	final var polled = consumer.poll(pollDurationSupplier.get());
+	if (polled.count() > 1) {
+	    LOGGER.atWarn().setMessage("Polled count: {}").addArgument(polled::count).log();
+	}
 
-                    if (invocable == null) {
-                        if (onUnmatched == null) {
-                            throw new UnknownEventException(event);
-                        } else {
-                            onUnmatched.accept(event);
-                        }
-                    } else {
-                        runnableBuilder.apply(invocable, event).run();
-                    }
-                } catch (Exception e) {
-                    LOGGER.atError().setCause(e)
-                            .setMessage(this.onException.getClass().getSimpleName()
-                                    + " failed, ignored: {}, {}, {} because of {}")
-                            .addArgument(event::topic).addArgument(event::key).addArgument(event::offset)
-                            .addArgument(e::getMessage).log();
+	StreamSupport.stream(polled.spliterator(), false).map(InboundEvent::new).forEach(this::dispatchEvent);
 
-                    if (this.onException != null) {
-                        this.onException.onException(new ConsumerExceptionListener.Context(consumer, event, e));
-                    }
-                }
-            });
+	if (polled.count() > 0) {
+	    consumer.commitSync();
+	}
+    }
 
-            if (polled.count() > 0) {
-                consumer.commitSync();
-            }
-        }
+    private void dispatchEvent(final InboundEvent event) {
+	try (final var closeble = EventMdcContext.set(event);) {
+	    this.onDispatching.stream().forEach(l -> l.onDispatching(event));
 
-        this.consumer.close();
-        this.closedFuture.complete(true);
+	    final var invocable = invocableFactory.get(event);
+
+	    if (invocable == null) {
+		if (onUnmatched == null) {
+		    throw new UnknownEventException(event);
+		} else {
+		    onUnmatched.accept(event);
+		}
+	    } else {
+		runnableBuilder.apply(invocable, event).run();
+	    }
+	} catch (Exception e) {
+	    LOGGER.atError().setCause(e)
+		    .setMessage(
+			    this.onException.getClass().getSimpleName() + " failed, ignored: {}, {}, {} because of {}")
+		    .addArgument(event::topic).addArgument(event::key).addArgument(event::offset)
+		    .addArgument(e::getMessage).log();
+
+	    if (this.onException != null) {
+		this.onException.onException(new ConsumerExceptionListener.Context(consumer, event, e));
+	    }
+	}
     }
 
     @Override
-    public Consumer<String, String> consumer() {
-        return this.consumer;
-    }
-
     public CompletableFuture<Boolean> close() {
-        this.closed = true;
-        return this.closedFuture;
+	this.closed = true;
+	this.consumer.wakeup();
+	return this.closedFuture;
     }
 }
--- src/main/java/me/ehp246/aufkafka/core/consumer/InboundEndpointConsumerConfigurer.java
+++ src/main/java/me/ehp246/aufkafka/core/consumer/InboundEndpointConsumerConfigurer.java
@@ -1,6 +1,6 @@
 package me.ehp246.aufkafka.core.consumer;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -36,64 +36,56 @@ public final class InboundEndpointConsumerConfigurer implements SmartInitializin
     private final String correlIdHeader;
 
     public InboundEndpointConsumerConfigurer(final List<InboundEndpoint> endpoints,
-            final InboundConsumerExecutorProvider executorProvider, final ConsumerProvider consumerProvider,
-            final EventInvocableBinder binder, final InboundDispatchingLogger inboundDispatchingLogger,
-            final AutowireCapableBeanFactory autowireCapableBeanFactory,
-            final DefaultInboundConsumerRegistry consumerRegistry,
-            @Value("${" + AufKafkaConstant.PROPERTY_HEADER_CORRELATIONID + ":" + AufKafkaConstant.CORRELATIONID_HEADER
-                    + "}") final String correlIdHeader) {
-        super();
-        this.endpoints = endpoints;
-        this.executorProvider = executorProvider;
-        this.consumerProvider = consumerProvider;
-        this.binder = binder;
-        this.onDispatching = inboundDispatchingLogger == null ? List.of() : List.of(inboundDispatchingLogger);
-        this.autowireCapableBeanFactory = autowireCapableBeanFactory;
-        this.consumerRegistry = consumerRegistry;
-        this.correlIdHeader = correlIdHeader;
+	    final InboundConsumerExecutorProvider executorProvider, final ConsumerProvider consumerProvider,
+	    final EventInvocableBinder binder, final InboundDispatchingLogger inboundDispatchingLogger,
+	    final AutowireCapableBeanFactory autowireCapableBeanFactory,
+	    final DefaultInboundConsumerRegistry consumerRegistry,
+	    @Value("${" + AufKafkaConstant.PROPERTY_HEADER_CORRELATIONID + ":" + AufKafkaConstant.CORRELATIONID_HEADER
+		    + "}") final String correlIdHeader) {
+	super();
+	this.endpoints = endpoints;
+	this.executorProvider = executorProvider;
+	this.consumerProvider = consumerProvider;
+	this.binder = binder;
+	this.onDispatching = inboundDispatchingLogger == null ? List.of() : List.of(inboundDispatchingLogger);
+	this.autowireCapableBeanFactory = autowireCapableBeanFactory;
+	this.consumerRegistry = consumerRegistry;
+	this.correlIdHeader = correlIdHeader;
     }
 
     @Override
     public void afterSingletonsInstantiated() {
-        for (final var endpoint : this.endpoints) {
-            LOGGER.atTrace().setMessage("Registering '{}' on '{}'").addArgument(endpoint::name)
-                    .addArgument(() -> endpoint.from().topic()).log();
+	for (final var endpoint : this.endpoints) {
+	    LOGGER.atTrace().setMessage("Registering '{}' on '{}'").addArgument(endpoint::name)
+		    .addArgument(() -> endpoint.from().topic()).log();
 
-            final var consumer = this.consumerProvider.get(endpoint.consumerConfigName(),
-                    endpoint.consumerProperties());
-            consumer.subscribe(Set.of(endpoint.from().topic()));
+	    final var consumer = this.consumerProvider.get(endpoint.consumerConfigName(),
+		    endpoint.consumerProperties());
+	    consumer.subscribe(Set.of(endpoint.from().topic()));
 
-            final var consumerRunner = new DefaultInboundEndpointConsumer(consumer, endpoint::pollDuration,
-                    new DefaultEventInvocableRunnableBuilder(this.binder,
-                            endpoint.invocationListener() == null ? null : List.of(endpoint.invocationListener())),
-                    new AutowireCapableInvocableFactory(autowireCapableBeanFactory, endpoint.invocableRegistry()),
-                    this.onDispatching, endpoint.unmatchedConsumer(), endpoint.consumerExceptionListener());
+	    final var consumerRunner = new DefaultInboundEndpointConsumer(consumer, endpoint::pollDuration,
+		    new DefaultEventInvocableRunnableBuilder(this.binder,
+			    endpoint.invocationListener() == null ? null : List.of(endpoint.invocationListener())),
+		    new AutowireCapableInvocableFactory(autowireCapableBeanFactory, endpoint.invocableRegistry()),
+		    this.onDispatching, endpoint.unmatchedConsumer(), endpoint.consumerExceptionListener());
 
-            this.consumerRegistry.put(endpoint.name(), consumerRunner);
-            this.executorProvider.get().execute(() -> {
-                EventMdcContext.setMdcHeaders(Set.of(this.correlIdHeader));
-                consumerRunner.run();
-                EventMdcContext.clearMdcHeaders();
-            });
-        }
+	    this.consumerRegistry.put(endpoint.name(), consumerRunner);
+	    this.executorProvider.get().execute(() -> {
+		EventMdcContext.setMdcHeaders(Set.of(this.correlIdHeader));
+		consumerRunner.run();
+		EventMdcContext.clearMdcHeaders();
+	    });
+	}
     }
 
     @Override
     public void close() throws Exception {
-        LOGGER.atTrace().setMessage("Closing consumers").log();
+	LOGGER.atTrace().setMessage("Closing consumers").log();
 
-        final var closedFuture = new ArrayList<CompletableFuture<Boolean>>(this.consumerRegistry.getNames().size());
+	final var closedFutures = new HashSet<CompletableFuture<Boolean>>(this.consumerRegistry.getNames().size());
 
-        for (String name : this.consumerRegistry.getNames()) {
-            if (!(this.consumerRegistry.get(name) instanceof DefaultInboundEndpointConsumer runner)) {
-                LOGGER.atWarn().setMessage("Unknown InboundConsumer of type: {}")
-                        .addArgument(this.consumerRegistry.get(name).consumer()).log();
-                continue;
-            }
+	this.consumerRegistry.getNames().forEach(name -> closedFutures.add(this.consumerRegistry.get(name).close()));
 
-            closedFuture.add(runner.close());
-        }
-
-        closedFuture.stream().forEach(CompletableFuture::join);
+	closedFutures.stream().forEach(CompletableFuture::join);
     }
 }
