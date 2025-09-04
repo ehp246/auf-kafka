705d882ced0417b563a7f8bee689b093f247b34f
Lei Yang <ehp246@yahoo.com>
2025-09-04 15:12:49 -0400

    Testing.

--- src/test/java/me/ehp246/test/embedded/consumer/enable/properties/PropertiesTest.java
+++ src/test/java/me/ehp246/test/embedded/consumer/enable/properties/PropertiesTest.java
@@ -1,12 +1,12 @@
 package me.ehp246.test.embedded.consumer.enable.properties;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.test.annotation.DirtiesContext;
 
 import me.ehp246.aufkafka.api.consumer.InboundEndpoint;
 import me.ehp246.test.mock.EmbeddedKafkaConfig;
@@ -16,27 +16,27 @@ import me.ehp246.test.mock.EmbeddedKafkaConfig;
  *
  */
 @SpringBootTest(classes = { EmbeddedKafkaConfig.class, AppConfig.class }, properties = {
-		"value.2=75f90fa4-da65-44de-bb59-d42a92448d7b" })
+        "value.2=75f90fa4-da65-44de-bb59-d42a92448d7b" })
 @EmbeddedKafka(topics = { "embedded" }, partitions = 1)
-@DirtiesContext
+@Disabled
 class PropertiesTest {
-	@Autowired
-	private ListableBeanFactory beanFactory;
+    @Autowired
+    private ListableBeanFactory beanFactory;
 
-	@Test
-	void properties_01() {
-		final var endpoint = beanFactory.getBeansOfType(InboundEndpoint.class).get("inboundEndpoint-0");
+    @Test
+    void properties_01() {
+        final var endpoint = beanFactory.getBeansOfType(InboundEndpoint.class).get("inboundEndpoint-0");
 
-		Assertions.assertEquals(0, endpoint.consumerProperties().size());
-	}
+        Assertions.assertEquals(0, endpoint.consumerProperties().size());
+    }
 
-	@Test
-	void properties_02() {
-		final var custom = beanFactory.getBeansOfType(InboundEndpoint.class).get("inboundEndpoint-1")
-				.consumerProperties();
+    @Test
+    void properties_02() {
+        final var custom = beanFactory.getBeansOfType(InboundEndpoint.class).get("inboundEndpoint-1")
+                .consumerProperties();
 
-		Assertions.assertEquals(2, custom.size());
-		Assertions.assertEquals("value1", custom.get("custom1"));
-		Assertions.assertEquals("75f90fa4-da65-44de-bb59-d42a92448d7b", custom.get("custom2"));
-	}
+        Assertions.assertEquals(2, custom.size());
+        Assertions.assertEquals("value1", custom.get("custom1"));
+        Assertions.assertEquals("75f90fa4-da65-44de-bb59-d42a92448d7b", custom.get("custom2"));
+    }
 }
